bc71a6e8d5dc2e906beb0d1b22d9fccccd578f0e
James <james.z.lucassen@gmail.com>
2025-05-09 19:16:20 -0400

    htb machine start/stop automation

--- htb.py
+++ htb.py
@@ -4,11 +4,11 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-# from selenium.common.exceptions import TimeoutException
+from selenium.common.exceptions import TimeoutException
 
 from dotenv import load_dotenv
 import os
-
+import time
 load_dotenv(override=True)
 
 # Set up Chrome webdriver
@@ -32,5 +32,29 @@ def login_to_htb():
     login_button.click()
     wait.until(EC.presence_of_element_located((By.ID, "topnavbar"))) # wait until login is complete
 
+def start_machine(machine_name):
+    driver.get(f"https://app.hackthebox.com/machines/{machine_name}")
+    try:
+        start_button = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, 'div.spawn-machine-button > div')))
+        start_button.click()
+    except TimeoutException:
+        # if machine was already started this will time out
+        pass
+    machine_ip = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, 'span.fontBold.color-green.cursorPointer')))
+    return machine_ip.text
+
+def stop_machine(machine_name):
+    driver.get(f"https://app.hackthebox.com/machines/{machine_name}")
+    try:
+        stop_button = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, 'i.icon-stop')))
+        stop_button.click()
+    except TimeoutException:
+        # if machine was already stopped this will time out
+        pass
+    wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, 'div.spawn-machine-button > div'))) # look for the start button again to confirm stop
+
 if __name__ == "__main__":
     login_to_htb()
+    print(start_machine("Cap"))
+    stop_machine("Cap")
+    breakpoint()
