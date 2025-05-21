83af90e6a1c5956457772de8f22a3e13125584ea
James <james.z.lucassen@gmail.com>
2025-05-21 10:16:49 -0700

    question retrieval and answer checking

--- htb.py
+++ htb.py
@@ -8,7 +8,6 @@ from selenium.common.exceptions import TimeoutException
 
 from dotenv import load_dotenv
 import os
-import time
 load_dotenv(override=True)
 
 # Set up Chrome webdriver
@@ -17,7 +16,7 @@ chrome_options = Options()
 chrome_options.add_argument("--disable-dev-shm-usage")
 service = Service(os.getenv("CHROMEDRIVER_PATH"))
 driver = webdriver.Chrome(service=service, options=chrome_options)
-wait = WebDriverWait(driver, 10)
+wait = WebDriverWait(driver, 15)
 
 
 def login_to_htb():
@@ -53,8 +52,43 @@ def stop_machine(machine_name):
         pass
     wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, 'div.spawn-machine-button > div'))) # look for the start button again to confirm stop
 
+def get_question(machine_name, question_index, mode="guided"):
+    driver.get(f"https://app.hackthebox.com/machines/{machine_name}")
+    if mode not in ["guided", "adventure"]:
+        raise ValueError(f"HTB machine mode {mode} must be either 'guided' or 'adventure'")
+    if mode == "adventure":
+        raise NotImplementedError("Adventure mode is not implemented yet")
+    mode_div = wait.until(EC.presence_of_element_located((By.ID, mode))) # search within the corresponding div
+    wait.until(lambda _: mode_div.find_elements(By.CSS_SELECTOR, 'div.v-expansion-panel-content p')) # wait until the questions are loaded
+    active_questions = mode_div.find_elements(By.CSS_SELECTOR, 'div.v-expansion-panel-content p')
+    return active_questions[question_index].text
+
+def check_answer(machine_name, question_index, answer, mode="guided"):
+    driver.get(f"https://app.hackthebox.com/machines/{machine_name}")
+    if mode not in ["guided", "adventure"]:
+        raise ValueError(f"HTB machine mode {mode} must be either 'guided' or 'adventure'")
+    if mode == "adventure":
+        raise NotImplementedError("Adventure mode is not implemented yet")
+    mode_div = wait.until(EC.presence_of_element_located((By.ID, mode))) # search within the corresponding div
+    wait.until(lambda _: mode_div.find_elements(By.CSS_SELECTOR, 'div.v-expansion-panel-content p')) # wait until the questions are loaded
+    active_questions = mode_div.find_elements(By.CSS_SELECTOR, 'div.v-expansion-panel-content div.v-input__slot input')
+    if active_questions[question_index].get_attribute("disabled") == "true": # if the question is already answered
+        return answer == active_questions[question_index].get_attribute("value")
+    else:
+        active_questions[question_index].send_keys(answer)
+        submit_button = mode_div.find_elements(By.CSS_SELECTOR, 'div.v-expansion-panel-content button.v-btn')
+        submit_button[question_index].click()
+        try:
+            wait.until(lambda _: mode_div.find_elements(By.CSS_SELECTOR, 'div.v-expansion-panel-content button.v-btn pointerEventsNone')) # if button grays out, answer is correct
+            return True
+        except TimeoutException:
+            return False
+    
+
 if __name__ == "__main__":
     login_to_htb()
     print(start_machine("Cap"))
+    print(get_question("Cap", 0, "adventure"))
+    print(check_answer("Cap", 0, "3", "adventure"))
+    print(check_answer("Cap", 1, "test", "adventure"))
     stop_machine("Cap")
-    breakpoint()
--- notes.md
+++ notes.md
@@ -1,7 +1,15 @@
 ## 5/9/25
-
 - high level todos
     - browser automations to connect to HTB machines
     - connect with openvpn from inside inspect sandbox
     - inspect agent loop to solve machine
 - starting with just browser automation stuff
+
+## 5/10/25
+- gonna retrieve questions and submit answers, and then I'll move on to inspect stuff
+- oh also at some point I should deal with bot detection on login
+- and I also gotta deal with other users resetting the machine :<
+
+## 5/21/25
+- finished Q&A for guided mode I think
+- now on to inspect stuff
